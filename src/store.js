987eb79310f6620dbef7621a8d1a79a6e12936ea
brennongs <brennonschow@gmail.com>
2017-11-28 09:43:12 -0700

    added comments to store.js

--- src/store.js
+++ src/store.js
@@ -1,4 +1,10 @@
-import { createStore, combineReducers } from 'redux'
+/*
+    STEP ONE - import dependencies and reducer
+*/
+import { createStore } from 'redux'
 import reducer from './ducks/reducer'
 
+/*
+    STEP TWO - create and export store
+*/
 export default createStore(reducer)
