4c6f19ebe5445171dce9bc4177f3bbb7a3d2a31c
brennongs <brennonschow@gmail.com>
2017-12-05 09:33:11 -0700

    adding promise middleware

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,19 +1,34 @@
+import axios from 'axios'
 /* 
     STEP ONE - set initial state & 
     add type constants 
 */
 const initialState = {
-        num: 0
-    }
-    , NEW_NUM = 'NEW_NUM'
+        num: 0,
+        quote: ''
+    },
+    NEW_NUM = 'NEW_NUM',
+    QUOTE = 'QUOTE'
 
 /*
     STEP TWO - create action creaters
 */
-export function makeNewNum(){
+export function makeNewNum() {
     return {
         type: NEW_NUM,
-        payload: ~~(Math.random()*10)
+        payload: ~~(Math.random() * 10)
+    }
+}
+
+export function getNewQuote() {
+    /*
+        to utilize redux-promise middleware, you make the payload your promise. 
+        middlware will fire and resolve the promise for you, and will update
+        the payload each step of the way.
+    */
+    return {
+        type: QUOTE,
+        payload: axios.get('https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en').then(q => q.data)
     }
 }
 
@@ -21,7 +36,7 @@ export function makeNewNum(){
     STEP THREE - create reducer function
     --ALL LOGIC FOR REDUX LIVES HERE--
 */
-export default function(state = initialState,  action){
+export default function (state = initialState, action) {
     /*
         @param {Object} action = {
             type: '',
@@ -30,8 +45,20 @@ export default function(state = initialState,  action){
     */
     switch (action.type) {
         case NEW_NUM:
-            return Object.assign({}, state, {num: action.payload})
-    
+            return Object.assign({}, state, {
+                num: action.payload
+            })
+
+        case QUOTE + '_PENDING':
+            return Object.assign({}, state, {
+                quote: 'pending'
+            })
+
+        case QUOTE + '_FULFILLED':
+            return Object.assign({}, state, {
+                quote: action.payload
+            })
+
         default:
             return state
     }
