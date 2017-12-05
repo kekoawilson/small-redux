b8db8359670b69b954a43a1de6ee4e12f2d7d250
brennongs <brennonschow@gmail.com>
2017-12-05 09:27:00 -0700

    adding promise middleware

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
 /*
     STEP ONE - import dependencies and reducer
 */
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import promiseMiddleware from 'redux-promise-middleware'
 import reducer from './ducks/reducer'
 
 /*
     STEP TWO - create and export store
+
+    *   createStore is used to actually create the store (go figure)
+    *   applyMiddleware is used to... apply middleware.
+    *   the order here is important
+
 */
-export default createStore(reducer)
+export default createStore(reducer, applyMiddleware(promiseMiddleware))
